e452addf5d6088a695337522a866622cc01dcbc6
Son Le <sonlexqt@gmail.com>
2016-08-17 23:43:59 +0700

    Pull real author data from postgreSQL db

--- api/sql/models.js
+++ api/sql/models.js
@@ -1,8 +1,17 @@
+import knex from './connector'
+import * as _ from 'lodash'
+
 export class Authors {
   getAuthorByName(firstName, lastName) {
-    return {
-      firstName: 'Son',
-      lastName: 'Le',
-    }
+    const query = knex('authors')
+      .where({
+        first_name: firstName,
+        last_name: lastName,
+      })
+    return query.then(([row]) => {
+      return _.mapKeys(row, (value, key) => {
+        return _.camelCase(key)
+      })
+    })
   }
 }
